87a73743a74be4610cb9e9da9803d7f1991c003a
Smith, Donnie <donnie.smith@rakuten.com>
2019-08-09 16:24:00 +0900

    tests: add tests for extra headers being added

--- Tests/Unit/ConfigFetcherTests.swift
+++ Tests/Unit/ConfigFetcherTests.swift
@@ -18,6 +18,13 @@ class ConfigFetcherSpec: QuickSpec {
     }
     override func spec() {
         describe("fetch function") {
+            let bundleMock = BundleMock()
+
+            // Fetcher will just return if these are invalid
+            // so need to set something valid
+            bundleMock.mockEndpoint = "https://www.endpoint.com"
+            bundleMock.mockAppId = "foo-id"
+
             it("will call the send function of the api client passing in a request") {
                 let apiClientMock = APIClientMock()
                 let fetcher = Fetcher(client: apiClientMock, environment: Environment())
@@ -43,7 +50,6 @@ class ConfigFetcherSpec: QuickSpec {
             }
             it("will pass nil in the completion handler when environment is incorrectly configured") {
                 var testResult: Any?
-                let bundleMock = BundleMock()
                 bundleMock.mockEndpoint = "12345"
                 let fetcher = Fetcher(client: APIClientMock(), environment: Environment(bundle: bundleMock))
 
@@ -56,13 +62,93 @@ class ConfigFetcherSpec: QuickSpec {
 
             it("will prefix ras- to the request's subscription key header") {
                 let apiClientMock = APIClientMock()
-                let fetcher = Fetcher(client: apiClientMock, environment: Environment())
+                bundleMock.mockSubKey = "my-subkey"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
+
+                fetcher.fetchConfig(completionHandler: { (_) in
+                    print(apiClientMock.request?.allHTTPHeaderFields)
+                })
+
+                expect(apiClientMock.request?.allHTTPHeaderFields!["apiKey"]).toEventually(equal("ras-my-subkey"))
+            }
+
+            it("will add the app id header") {
+                let apiClientMock = APIClientMock()
+                bundleMock.mockAppId = "my-app-id"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
 
                 fetcher.fetchConfig(completionHandler: { (_) in
                 })
 
-                expect(apiClientMock.request?.allHTTPHeaderFields!["apiKey"]).toEventually(contain("ras-"))
+                expect(apiClientMock.request?.allHTTPHeaderFields!["ras-app-id"]).toEventually(equal("my-app-id"))
             }
+
+            it("will add the app name header") {
+                let apiClientMock = APIClientMock()
+                bundleMock.mockAppName = "my-app-name"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
+
+                fetcher.fetchConfig(completionHandler: { (_) in
+                })
+
+                expect(apiClientMock.request?.allHTTPHeaderFields!["ras-app-name"]).toEventually(equal("my-app-name"))
+            }
+
+            it("will add the app version header") {
+                let apiClientMock = APIClientMock()
+                bundleMock.mockAppVersion = "100.1.0"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
+
+                fetcher.fetchConfig(completionHandler: { (_) in
+                })
+
+                expect(apiClientMock.request?.allHTTPHeaderFields!["ras-app-version"]).toEventually(equal("100.1.0"))
+            }
+
+            it("will add the device model header") {
+                let apiClientMock = APIClientMock()
+                bundleMock.mockDeviceModel = "a-model"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
+
+                fetcher.fetchConfig(completionHandler: { (_) in
+                })
+
+                expect(apiClientMock.request?.allHTTPHeaderFields!["ras-device-model"]).toEventually(equal("a-model"))
+            }
+
+            it("will add the device OS version header") {
+                let apiClientMock = APIClientMock()
+                bundleMock.mockOsVersion = "os foo"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
+
+                fetcher.fetchConfig(completionHandler: { (_) in
+                })
+
+                expect(apiClientMock.request?.allHTTPHeaderFields!["ras-device-version"]).toEventually(equal("os foo"))
+            }
+
+            it("will add the sdk name header") {
+                let apiClientMock = APIClientMock()
+                bundleMock.mockSdkName = "my sdk"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
+
+                fetcher.fetchConfig(completionHandler: { (_) in
+                })
+
+                expect(apiClientMock.request?.allHTTPHeaderFields!["ras-sdk-name"]).toEventually(equal("my sdk"))
+            }
+
+            it("will add the sdk version header") {
+                let apiClientMock = APIClientMock()
+                bundleMock.mockSdkVersion = "1.2.3"
+                let fetcher = Fetcher(client: apiClientMock, environment: Environment(bundle: bundleMock))
+
+                fetcher.fetchConfig(completionHandler: { (_) in
+                })
+
+                expect(apiClientMock.request?.allHTTPHeaderFields!["ras-sdk-version"]).toEventually(equal("1.2.3"))
+            }
+
             context("when error is received as the result from the api client") {
                 it("will pass nil in the completion handler") {
                     var testResult: Any?
