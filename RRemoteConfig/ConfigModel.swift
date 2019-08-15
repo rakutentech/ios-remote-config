e10a9fda88af1cce7b6872f51fb47b5574c7d40d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-15 07:55:01 +0000

    Deploy rakutentech/ios-remote-config to github.com/rakutentech/ios-remote-config.git:gh-pages

--- RRemoteConfig/ConfigModel.swift
+++
@@ -1,7 +0,0 @@
-internal struct ConfigModel: Decodable {
-    let config: [String: String]
-
-    enum CodingKeys: String, CodingKey {
-        case config = "body"
-    }
-}
--- RRemoteConfig/RemoteConfig.swift
+++
@@ -1,6 +0,0 @@
-@objc public class RemoteConfig: NSObject {
-
-    @objc public class func getString(key: String, fallback: String) -> String {
-        return RealRemoteConfig.shared.getString(key, fallback)
-    }
-}
