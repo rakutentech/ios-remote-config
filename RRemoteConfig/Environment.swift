35e12bc17de52c27f6384a86a3a74d7ea6d67d44
Donnie Smith <donnie.smith@rakuten.com>
2020-04-06 19:36:04 +0900

    improve: add query params to support the conditions feature to the Config API request SDKCF-1792

--- RRemoteConfig/Environment.swift
+++ RRemoteConfig/Environment.swift
@@ -18,11 +18,26 @@ internal class Environment {
         return URL(string: "\(endpointUrlString)")
     }
     var configUrl: URL? {
-        guard let appId = bundle.value(for: "RASApplicationIdentifier") else {
+        guard let appId = bundle.value(for: "RASApplicationIdentifier"),
+            let path = baseUrl?.appendingPathComponent("/app/\(appId)/config"),
+            var components = URLComponents(url: path, resolvingAgainstBaseURL: true) else {
             Logger.e("Ensure RASApplicationIdentifier value in plist is valid")
             return nil
         }
-        return baseUrl?.appendingPathComponent("/app/\(appId)/config")
+        // GET /api/v1/app/{appId}/config?appVersion={appVersion}&osVersion={osVersion}&language={language}&country={country}
+        var queryItems: [URLQueryItem] = [URLQueryItem]()
+        queryItems.append(URLQueryItem(name: "appVersion", value: appVersion))
+        queryItems.append(URLQueryItem(name: "osVersion", value: osVersion))
+
+        if let language = Locale.current.languageCode {
+            queryItems.append(URLQueryItem(name: "language", value: language))
+        }
+
+        if let country = Locale.current.regionCode {
+            queryItems.append(URLQueryItem(name: "country", value: country))
+        }
+        components.queryItems = queryItems
+        return components.url
     }
     var subscriptionKey: String {
         return bundle.value(for: "RASProjectSubscriptionKey") ?? bundle.valueNotFound
